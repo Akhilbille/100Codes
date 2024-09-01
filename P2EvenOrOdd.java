d8c60957c80963ce4002a7b11c38cdae289c19b9
Akhil Bille <akhilgowd829@gmail.com>
2024-09-01 10:20:14 -0400

    P2EvenOrOdd Completed

---
+++ P2EvenOrOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P2EvenOrOdd {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        //Brute Method
+        // if(num%2 == 0) System.out.println("Even");
+        // else System.out.println("Odd");
+
+        //Ternary Operator
+        String result = num%2==0?"Even":"Odd";
+        System.out.println(result);
+
+        input.close();
+    }
+}
