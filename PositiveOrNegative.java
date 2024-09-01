658fad625f49bab1cf186ef21b5216ccda9c2791
Akhil Bille <akhilgowd829@gmail.com>
2024-09-01 10:06:49 -0400

    P1PositiveOrNegative Completed

--- PositiveOrNegative.java
+++ PositiveOrNegative.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class PositiveOrNegative {
 
     public static void main(String[] args) {
+        int num;
+        Scanner input = new Scanner(System.in);
+        num= input.nextInt();
+
+        //Brute Method
+        // if(num==0){
+        //     System.out.println("Zero");
+        // }
+        // else if (num > 0)
+        //     System.out.println("Positive");
+        // else
+        //     System.out.println("Negative");
+
+        //Ternary Method
+        String result = num>=0?(num==0?"Zero":"Positive"):"Negative";
+        System.out.println(result);
+        input.close();
+
 
     }
 }
